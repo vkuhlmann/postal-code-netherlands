aa6565fd9b41ddc808271a765bd7bc3bce5efc3a
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2025-07-21 23:49:52 +0200

    feat: add workflow for pushing to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Set basePath for GitHub Pages (will be repository name)
+  basePath: process.env.NODE_ENV === 'production' ? '/postal-code-netherlands' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/postal-code-netherlands' : '',
 };
 
 export default nextConfig;
